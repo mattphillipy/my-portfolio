3d78088b528af73915bc442aa93cced492077cdd
Matt Phillipy <mdss@hotmail.com>
2017-09-15 16:39:36 -0700

    lots of files

---
+++ js/example-work.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class ExampleWork extends React.Component {
+  render() {
+    return (
+      <section className="section section--alignCentered section--description">
+
+        { this.props.work.map( (example, idx) => {
+            return (
+              <ExampleWorkBubble example={example}/>
+            )
+          })
+        }
+
+      </section>
+    )
+  }
+}
+
+class ExampleWorkBubble extends React.Component {
+  render() {
+    return (
+      <div className="section__exampleWrapper">
+        <div className="section__example">
+          <img alt="example screenshot of a project involving code"
+               className="section__exampleImage"
+               src="images/example1.png"/>
+          <dl className="color--cloud">
+            <dt className="section__exampleTitle section__text--centered">
+              Work Example
+            </dt>
+            <dd></dd>
+          </dl>
+        </div>
+      </div>
+    )
+  }
+}
+export default ExampleWork;
