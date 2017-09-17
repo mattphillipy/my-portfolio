c8689dfeb3febb8896879929aa7aa64ab557cce5
Matt Phillipy <mdss@hotmail.com>
2017-09-17 09:26:42 -0700

    everythint

--- js/example-work-modal.js
+++ js/example-work-modal.js
@@ -8,7 +8,7 @@ class ExampleWorkModal extends React.Component {
 
     return (
       <div className={"background--skyBlue " + modalClass}>
-        <span className="color--cloud modal__closeButton">
+        <span className="color--cloud modal__closeButton"
           onClick={this.props.closeModal}>
           <i className="fa fa-window-close-o"></i>
         </span>
